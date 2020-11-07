555036e752e56bc8ee053b79ba7f19f2b4e44ef8
jglisczinski <71847380+jglisczinski@users.noreply.github.com>
2020-11-06 19:52:52 -0600

    Update DataWrangler.java

--- jglisczinski_Data_Wrangler/DataWrangler.java
+++ jglisczinski_Data_Wrangler/DataWrangler.java
@@ -1,3 +1,12 @@
+// --== CS400 File Header Information ==--
+// Name: Jackson Glisczinski
+// Email: jglisczinski@wisc.edu
+// Team: GE
+// Role: Data Wrangler
+// TA: Dan Kiel
+// Lecturer: Gary Dahl
+// Notes to Grader: I also created the txt files: "airport.txt" and "flights.txt"
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -5,11 +14,25 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class offers static load and save methods, which is to be used with the Back End class.
+ * 
+ * @author jackson
+ */
 public class DataWrangler {
 
+  // String = key (IATA code), Airport = value (Airport Object w/ other info)
   private static Hashtable<String, Airport> airports = new Hashtable<String, Airport>();
 
+  /**
+   * First loads airports into the graph based on a txt file holding 45 airports. Then loads edges
+   * connecting the airports with weight representing flight cost ($). International flights are
+   * more expensive than domestic flights.
+   * 
+   * @param graph holds airports and flights
+   */
   public static void load(CS400Graph<String> graph) {
+    // import airports (vertices)
     try {
       File airportsFile = new File("airports.txt");
       Scanner in = new Scanner(airportsFile);
@@ -30,18 +53,36 @@ public class DataWrangler {
       System.out.println(q.getMessage());
       q.printStackTrace();
     }
+    // import flights (edges)
+    try {
+      File flightsFile = new File("flights.txt");
+      Scanner in = new Scanner(flightsFile);
+      while (in.hasNextLine()) {
+        String[] currLine = in.nextLine().trim().split(",");
+        String start = currLine[0].trim();
+        String end = currLine[1].trim();
+        int cost = Integer.parseInt(currLine[2].trim());
+        graph.insertEdge(start, end, cost);
 
-
-    // for each airport in the text file
-    // airports.put(iataCode, airport);
-    // graph.insertVertex(airport);
-
-    // for each pair of iataCodes with weight
-    // airports.insertEdge(iataStart, iataEnd, cost);
-
+      }
+      in.close();
+    } catch (FileNotFoundException e) {
+      System.out.println(("File flights.txt not found."));
+    } catch (Exception q) {
+      System.out.println(q.getMessage());
+      q.printStackTrace();
+    }
 
   } // end load
 
+  /**
+   * Writes info to a file representing a ticket. Based on input, displays starting point,
+   * destination, cost, and a breakdown of the path.
+   * 
+   * @param graph holds airports and flights
+   * @param start Airport to start path from
+   * @param end   Airport to end path at
+   */
   public static void saveTicket(CS400Graph<String> graph, String start, String end) {
     List<String> airportSequence = graph.shortestPath(start, end);
     try {
@@ -76,8 +117,14 @@ public class DataWrangler {
     } catch (Exception q) {
       q.printStackTrace();
     }
-  }
+  } // end saveTicket
 
+  /**
+   * Returns the toString method of the airport based on the given iataCode. Else returns null.
+   * 
+   * @param iataCode
+   * @return
+   */
   public static String airportToString(String iataCode) {
     if (airports.containsKey(iataCode)) {
       return airports.get(iataCode).toString();
